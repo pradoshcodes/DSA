4d16f134323a0423ea9d685bee46eac109c38f8a
pradoshcodes <pradoshmohanty034@gmail.com>
2024-07-25 10:21:16 +0530

    first commit

---
+++ ArrayInsert.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayInsert {
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of the array: ");
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    System.out.println("Enter the elements of the array: ");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+        }
+      for(int i=0;i<n;i++){
+        System.out.println("Arrays :"+arr[i]+" ");
+      }
+
+    }
+}
